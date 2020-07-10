8da6354723882a74fea987960b84f5e36aa02da4
Armando Carrillo <ingcompuarmando@gmail.com>
2020-07-09 23:44:46 -0500

    Replace viewController to tableViewController

--- FavoriteBook/BookFormTableViewController.swift
+++ FavoriteBook/BookFormTableViewController.swift
@@ -12,7 +12,11 @@ class BookFormTableViewController: UITableViewController {
     @IBOutlet weak var authorTextField: UITextField!
     @IBOutlet weak var genreTextField: UITextField!
     @IBOutlet weak var lenghTextField: UITextField!
+    var book : Book?
     
+    struct  PropertyKeys {
+        static let unwind = "UnwindToBookTable"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +28,19 @@ class BookFormTableViewController: UITableViewController {
     }
 
     @IBAction func saveButton(_ sender: UIButton) {
+        guard let title = titleTextField.text, let author = authorTextField.text, let genre = genreTextField.text, let length = lenghTextField.text else { return }
+        book = Book(title: title, author: author, genre: genre, length: length)
+        performSegue(withIdentifier: PropertyKeys.unwind, sender: self)
+    }
+   
+    func updateView(){
+        guard let book = book else {return}
+        
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lenghTextField.text = book.length
+        
     }
     
 
